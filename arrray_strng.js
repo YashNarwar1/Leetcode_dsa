697455e7fd4e17e4fcd0c48a73db61b8c7378a03
Yash Narwar <yashnarwar111@gmail.com>
2025-04-03 15:22:30 +0530

    second commit with two string question

--- arrray_strng.js
+++ arrray_strng.js
@@ -22,3 +22,53 @@ const findClosestToZero = (array) => {
     return closest;
   }
 };
+
+// question 1768 merge string alternatively
+// given two strings you have to add their letter alternate
+// if one string is short and only one string have letter left than push them in the end
+
+// solution
+const mergeAlternate = (word1, word2) => {
+  const A = word1.length,
+    B = word2.length;
+  let a = 0;
+  let b = 0;
+  let result = "";
+
+  while (a < A && b < B) {
+    result += word1[a++];
+    result += word2[b++];
+  }
+
+  while (a < A) {
+    result += word1[a++];
+  }
+
+  while (b < B) {
+    result += word2[b++];
+  }
+
+  return result;
+};
+
+// Question 3 = roman to integer
+//leetcode 13
+// you are given a string of roman number conver it to integer number
+
+const converToInt = (s) => {
+  const d = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
+  let summ = 0;
+  const n = s.length;
+  let i = 0;
+
+  while (i < n) {
+    if (i < n - 1 && d[s[i]] < d[s[i + 1]]) {
+      summ += d[s[i + 1]] - d[s[i]];
+      i += 2;
+    } else {
+      summ += d[s[i]];
+    }
+  }
+
+  return summ;
+};
