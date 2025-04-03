69c1acb92a45b3b32a222020ea448dcea53348db
Yash Narwar <yashnarwar111@gmail.com>
2025-04-03 14:21:49 +0530

    Initial commit of JS file

---
+++ arrray_strng.js
@@ -0,0 +1,4 @@
+// nearest to the zero
+// problem you have an array of number and you need to find the number nearest to zero if there is a tie then return the bigger one
+// the array have both positive and negative number
+// so if you have -2 and 2 then return 2 as an result
